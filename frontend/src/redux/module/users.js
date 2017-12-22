954725b8406925dbce2700ebf31dbac30e0b1537
jinjaeeon <momentjin@gmail.com>
2017-12-23 01:48:48 +0900

    setting up the redux store with multiple reduces

--- frontend/src/redux/module/users.js
+++ frontend/src/redux/module/users.js
@@ -6,10 +6,19 @@
 
 // initial state
 const initialState = {
-    
+    // local storage는 쿠키와 유사함.
+    isLoggedIn: localStorage.getItem("jwt") || false
 }
+
 // reducer
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
 
 // exports
 
 // reducer export
+export default reducer;
