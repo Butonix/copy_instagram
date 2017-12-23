bdb3bc7d17c52e2b95d2d0ce8fcdd717de7aad5b
jinjaeeon <momentjin@gmail.com>
2017-12-23 17:13:41 +0900

    add package 'i18n' for multi language

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { i18nState } from "redux-i18n";
 import users from "redux/module/users";
 // import Reactotron from "ReactotronConfig";
 
@@ -27,7 +28,8 @@ if(env === "development") {
 
 const reducer = combineReducers ({
     users,
-    routing: routerReducer
+    routing: routerReducer,
+    i18nState
 });
 
 // ...은 array를 unpack하는 것
