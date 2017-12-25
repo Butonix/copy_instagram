f80ac3913cd5b9719fb063b433d69f66fe8652bd
jinjaeeon <momentjin@gmail.com>
2017-12-26 03:58:22 +0900

    controlling inputs and submit on LoginForm

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginForm from "./presenter";
 
-const Container = props => <LoginForm {...props} />
+class Container extends Component {
+    
+    state = {
+        username: "",
+        password: ""
+    };
+    
+    
+    render() {
+        const { username, password } = this.state;
+        return (
+             <LoginForm 
+                // 함수를 props로 넘기고 있다..!
+                handleInputChange={this._handleInputChange} 
+                handleSubmit={this._handleSubmit}
+                usernameValue={username} 
+                passwordValue={password}
+            />
+        );
+    }
+
+    _handleInputChange = event => {
+        const { target : { value, name} } = event;
+        this.setState({
+            [name]: value // [name]은 이벤트 발생시 반환되는 username, password 를 뜻함.
+        });
+    };
+
+    _handleSubmit = event => {
+        // 브라우저가 디폴트 작업을 수행하지 않도록 막는 함수
+        // <input type button으로 막는 것이 아닌, 이 문장을 씀으로써 submit으로
+        // 기존 동작을 거부할 수 있다.
+        event.preventDefault();
+        console.log(this.state);
+    }
+}
 
 export default Container;
--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -6,14 +6,28 @@ import PropTypes from "prop-types";
 
 export const LoginForm = (props, context) => (
     <div className={formStyles.formComponent}>
-        <form className={formStyles.form}>
-            <input type="text" placeholder={context.t("Username")} className={formStyles.textInput} />
+        <form className={formStyles.form} onSubmit={props.handleSubmit}> 
+            <input 
+                type="text"
+                placeholder={context.t("Username")}
+                className={formStyles.textInput}
+                value={props.usernameValue}
+                onChange={props.handleInputChange}
+                name="username"
+            />
             <input
                 type="password"
                 placeholder={context.t("Password")}
                 className={formStyles.textInput}
+                value={props.passwordValue}
+                onChange={props.handleInputChange}
+                name="password"
             />
-            <input type="submit" value={context.t("Log in")} className={formStyles.button} />
+            <input
+                type="submit"
+                value={context.t("Log in")}
+                className={formStyles.button}
+            />
         </form>
         <span className={formStyles.divider}>or</span>
         <span className={formStyles.facebookLink}>
@@ -22,6 +36,13 @@ export const LoginForm = (props, context) => (
         <span className={formStyles.forgotLink}> {context.t("Forgot password?")} </span>
     </div>
 );
+
+LoginForm.propTypes = {
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+};
 
 LoginForm.contextTypes = {
     t: PropTypes.func.isRequired
