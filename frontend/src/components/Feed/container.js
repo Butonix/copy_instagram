ef87bc326bf1f76396050524e3d2e31aa2dcda76
jinjaeeon <momentjin@gmail.com>
2017-12-27 00:20:07 +0900

    add component 'RenderFeed'

--- frontend/src/components/Feed/container.js
+++ frontend/src/components/Feed/container.js
@@ -9,7 +9,8 @@ class Container extends Component {
     };
 
     static propTypes = {
-        getFeed: PropTypes.func.isRequired
+        getFeed: PropTypes.func.isRequired,
+        feed: PropTypes.array // 항상 요구되는 것은 아니다.
     };
 
     componentDidMount() {
@@ -17,8 +18,24 @@ class Container extends Component {
         getFeed();
     } 
 
+    // 이것도 내장 함수로 존재.
+    // 로딩중을 끝내려면 새로운 Props가 들어온 것을 확인해야 한다.
+    // 이 떄 사용하는 것이 아래의 함수다.
+    componentWillReceiveProps = (nextProps) => {
+        // 초기 상태와 다음 상태의 비교를 통해 새로운 Props가 들어왔는지 검사한다.
+        // console.log(this.props); 초기 상태
+        // console.log(nextProps); 다음 상태
+
+        if( nextProps.feed) {
+            this.setState({
+                loading: false
+            });
+        }
+    };
+
     render() {
-        return <Feed {...this.state} />
+        const { feed } = this.props;
+        return <Feed {...this.state} feed={feed}/>
     }
 }
 
--- frontend/src/redux/module/photos.js
+++ frontend/src/redux/module/photos.js
@@ -3,8 +3,15 @@ import { actionCreators as userActions } from "redux/module/user";
 
 
 // actions
+const SET_FEED = "SET_FEED";
 
 // action creators
+function setFeed(feed) {
+    return {
+        type: SET_FEED,
+        feed
+    }
+}
 
 // api actions
 // 좀 다른 방식의 dispatch를 사용하고 있다(user와 비교해보자)
@@ -26,26 +33,34 @@ function getFeed() {
                 return response.json();
             }
         })
-        .then(json => console.log(json))
+        .then(json => dispatch(setFeed(json)))
         .catch(err => console.log(err))
     };
 }
 
 
 // initial state
-const initialState = {
-    feed: []
-};
+const initialState = {};
 
 // reducer
 function reducer(state = initialState, action) {
     switch(action.type) {
+        case SET_FEED:
+            return applySetFeed(state, action);
         default:
             return state;
     }
 }
 
 // reducer functions
+function applySetFeed(state, action) {
+    const { feed } = action;
+    return {
+        ...state,
+        feed
+    }
+}
+
 
 // action creators
 const actionCreators = {
