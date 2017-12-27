8f16d63c17081e6ddf9ba96a35102a307d6bbb79
jinjaeeon <momentjin@gmail.com>
2017-12-27 17:50:12 +0900

    add component 'UserList'

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -5,6 +5,7 @@ import PhotoActions from "components/PhotoActions";
 import PhotoComments from "components/PhotoComments";
 import TimeStamp from "components/TimeStamp";
 import CommentBox from "components/CommentBox";
+import UserList from "components/UserList";
 
 
 const FeedPhoto = (props, context) => {
@@ -27,6 +28,7 @@ const FeedPhoto = (props, context) => {
                     number={props.like_count}
                     isLiked={props.is_liked}
                     photoId={props.id}
+                    openLikes={props.openLikes}
                 />
                 <PhotoComments
                     caption={props.caption}
@@ -38,10 +40,21 @@ const FeedPhoto = (props, context) => {
                 />
                 <CommentBox photoId={props.id}/>
             </div>
+            
+            { props.seeingLikes && (
+                 <UserList 
+                    title={context.t("Likes")} 
+                    closeLikes={props.closeLikes}
+                />)
+            }          
+
         </div>
     );
 };
 
+FeedPhoto.contextTypes = {
+    t: PropTypes.func.isRequired
+};
 
 FeedPhoto.propTypes = {
     creator: PropTypes.shape({
@@ -65,7 +78,10 @@ FeedPhoto.propTypes = {
     ).isRequired,
 
     natural_time: PropTypes.string.isRequired,
-    is_liked: PropTypes.bool.isRequired
+    is_liked: PropTypes.bool.isRequired,
+    seeingLikes: PropTypes.bool.isRequired,
+    closeLikes: PropTypes.func.isRequired,
+    openLikes: PropTypes.func.isRequired
 };
 
 
